48760396e2a1c4beab03977ee38b8704575930dd
ADMIN <ADMIN@LAPTOP-50UAQEI0>
2023-03-14 21:50:04 +0700

    Add project files.

---
+++ PhamHoangNam_2080600040/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhamHoangNam_2080600040.MvcApplication" Language="C#" %>
---
+++ PhamHoangNam_2080600040/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhamHoangNam_2080600040.Startup))]
+namespace PhamHoangNam_2080600040
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
